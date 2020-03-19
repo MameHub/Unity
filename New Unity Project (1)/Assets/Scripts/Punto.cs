af5b8efbf6f4c518daaf5124f53d407af036fffb
MameHub <61141429+MameHub@users.noreply.github.com>
2020-03-19 15:33:05 +0100

    firstprojectUnity

---
+++ New Unity Project (1)/Assets/Scripts/Punto.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Punto : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerController>())
+        {
+            GameManager gameManager = FindObjectOfType<GameManager>();
+            gameManager.AnadirPunto();
+            Destroy(gameObject);
+        }
+    }
+}
