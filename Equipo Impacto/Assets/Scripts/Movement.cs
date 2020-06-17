24bed27e833d0eb52442280af35f737aff1f6e67
MameHub <61141429+MameHub@users.noreply.github.com>
2020-06-18 00:18:36 +0200

    intento

---
+++ Equipo Impacto/Assets/Scripts/Movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float rotationSpeed;
+    private Rigidbody rb;
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+    }
+    private Camera mainCamera;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        mainCamera = Camera.main;
+    }
+}
